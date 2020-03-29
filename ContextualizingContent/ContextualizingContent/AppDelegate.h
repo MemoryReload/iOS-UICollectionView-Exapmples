e07195b910874860ad6ff119b50fa4e81920b1b0
MemoryReload <heping_tsdwx@163.com>
2020-03-29 22:15:10 +0800

    the very first commit!

---
+++ ContextualizingContent/ContextualizingContent/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContextualizingContent
+//
+//  Created by HePing on 2020/3/29.
+//  Copyright © 2020 何平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ContextualizingContent/ContextualizingContent/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ContextualizingContent
+//
+//  Created by HePing on 2020/3/29.
+//  Copyright © 2020 何平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
