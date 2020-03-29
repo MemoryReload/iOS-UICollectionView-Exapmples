0eb9d0535b9658d024dcada2888d1f388c1ed6d9
MemoryReload <heping_tsdwx@163.com>
2020-03-29 22:36:36 +0800

    add AFModel

---
+++ ContextualizingContent/ContextualizingContent/AFPhotoModel.m
@@ -0,0 +1,13 @@
+//
+//  AFPhotoModel.m
+//  ContextualizingContent
+//
+//  Created by HePing on 2020/3/29.
+//  Copyright © 2020 何平. All rights reserved.
+//
+
+#import "AFPhotoModel.h"
+
+@implementation AFPhotoModel
+
+@end
