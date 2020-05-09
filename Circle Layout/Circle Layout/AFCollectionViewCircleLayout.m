ce2cc756c0651dc305a548179a282186611974d8
ping.he <ping.he@kuwo.cn>
2020-05-09 16:50:22 +0800

    delete and insert animation

--- Circle Layout/Circle Layout/AFCollectionViewCircleLayout.m
+++ Circle Layout/Circle Layout/AFCollectionViewCircleLayout.m
@@ -90,62 +90,64 @@ static NSString *AFCollectionViewFlowDecoration = @"DecorationView";
     return attributes;
 }
 
-//-(void)prepareForCollectionViewUpdates:(NSArray *)updateItems
-//{
-//    [super prepareForCollectionViewUpdates:updateItems];
-//    
-//    [updateItems enumerateObjectsUsingBlock:^(UICollectionViewUpdateItem *updateItem, NSUInteger idx, BOOL *stop) {
-//        if (updateItem.updateAction == UICollectionUpdateActionInsert)
-//        {
-//            [self.insertedRowSet addObject:@(updateItem.indexPathAfterUpdate.item)];
-//        }
-//        else if (updateItem.updateAction == UICollectionUpdateActionDelete)
-//        {
-//            [self.deletedRowSet addObject:@(updateItem.indexPathBeforeUpdate.item)];
-//        }
-//    }];
-//}
-//
-//-(void)finalizeCollectionViewUpdates
-//{
-//    [super finalizeCollectionViewUpdates];
-//    
-//    [self.insertedRowSet removeAllObjects];
-//    [self.deletedRowSet removeAllObjects];
-//}
-//
-//- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
-//{
-//    UICollectionViewLayoutAttributes *attributes = [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
-//    
-//    if ([self.insertedRowSet containsObject:@(itemIndexPath.item)])
-//    {
-//        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
-//        attributes.alpha = 0.0;
-//        attributes.center = self.center;
-//        return attributes;
-//    }
-//    
-//    return attributes;
-//}
-//
-//- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
-//{
-//    // The documentation says that this returns nil. It is lying. 
-//    UICollectionViewLayoutAttributes *attributes = [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
-//    
-//    if ([self.deletedRowSet containsObject:@(itemIndexPath.item)])
-//    {
-//        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
-//        attributes.alpha = 0.0;
-//        attributes.center = self.center;
-//        attributes.transform3D = CATransform3DConcat(CATransform3DMakeRotation((2 * M_PI * itemIndexPath.item / (self.cellCount + 1)), 0, 0, 1), CATransform3DMakeScale(0.1, 0.1, 1.0));
-//        
-//        return attributes;
-//    }
-//    
-//    return attributes;
-//}
+-(void)prepareForCollectionViewUpdates:(NSArray *)updateItems
+{
+    [super prepareForCollectionViewUpdates:updateItems];
+    
+    [updateItems enumerateObjectsUsingBlock:^(UICollectionViewUpdateItem *updateItem, NSUInteger idx, BOOL *stop) {
+        if (updateItem.updateAction == UICollectionUpdateActionInsert)
+        {
+            [self.insertedRowSet addObject:@(updateItem.indexPathAfterUpdate.item)];
+        }
+        else if (updateItem.updateAction == UICollectionUpdateActionDelete)
+        {
+            [self.deletedRowSet addObject:@(updateItem.indexPathBeforeUpdate.item)];
+        }
+    }];
+}
+
+-(void)finalizeCollectionViewUpdates
+{
+    [super finalizeCollectionViewUpdates];
+    
+    [self.insertedRowSet removeAllObjects];
+    [self.deletedRowSet removeAllObjects];
+}
+
+- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    UICollectionViewLayoutAttributes *attributes = [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
+    
+    if ([self.insertedRowSet containsObject:@(itemIndexPath.item)])
+    {
+        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+        attributes.alpha = 0.0;
+        attributes.center = self.center;
+        return attributes;
+    }
+    
+    return attributes;
+}
+
+- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    // The documentation says that this returns nil. It is lying.
+    UICollectionViewLayoutAttributes *attributes = [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
+    
+    if ([self.deletedRowSet containsObject:@(itemIndexPath.item)])
+    {
+        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+        attributes.alpha = 0.0;
+        attributes.center = self.center;
+        /*
+         There is a little bit tricky here. As the data source for this cell has been removed, the self.cellCount has been decreased. But the UI hasn't been updated yet. So, don't try to simply scale down the attributes with factor 0.1, because the rotation is wrong caculated. Thus we should recaculate the rotation radius with self.cellCount+1.
+         */
+        attributes.transform3D = CATransform3DConcat(CATransform3DMakeRotation((2 * M_PI * itemIndexPath.item / (self.cellCount + 1)), 0, 0, 1), CATransform3DMakeScale(0.1, 0.1, 1.0));
+        return attributes;
+    }
+    
+    return attributes;
+}
 
 #pragma mark Decoration View Layout
 
--- Circle Layout/Circle Layout/AFCollectionViewFlowLayout.m
+++ Circle Layout/Circle Layout/AFCollectionViewFlowLayout.m
@@ -35,62 +35,62 @@
 
 #pragma mark - Overridden Methods
 
-//-(void)prepareForCollectionViewUpdates:(NSArray *)updateItems
-//{
-//    [super prepareForCollectionViewUpdates:updateItems];
-//    
-//    [updateItems enumerateObjectsUsingBlock:^(UICollectionViewUpdateItem *updateItem, NSUInteger idx, BOOL *stop) {
-//        if (updateItem.updateAction == UICollectionUpdateActionInsert)
-//        {
-//            [self.insertedRowSet addObject:@(updateItem.indexPathAfterUpdate.item)];
-//        }
-//        else if (updateItem.updateAction == UICollectionUpdateActionDelete)
-//        {
-//            [self.deletedRowSet addObject:@(updateItem.indexPathBeforeUpdate.item)];
-//        }
-//    }];
-//}
-//
-//-(void)finalizeCollectionViewUpdates
-//{
-//    [super finalizeCollectionViewUpdates];
-//    
-//    [self.insertedRowSet removeAllObjects];
-//    [self.deletedRowSet removeAllObjects];
-//}
-//
-//- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
-//{
-//    UICollectionViewLayoutAttributes *attributes = [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
-//    
-//    if ([self.insertedRowSet containsObject:@(itemIndexPath.item)])
-//    {
-//        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
-//        attributes.alpha = 0.0;
-//        attributes.transform3D = CATransform3DMakeScale(0.1, 0.1, 1.0);
-//        attributes.transform3D = CATransform3DRotate(attributes.transform3D, -M_PI_4, 0, 0, 1);
-//        return attributes;
-//    }
-//    
-//    return attributes;
-//}
-//
-//- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
-//{
-//    // The documentation says that this returns nil. It is lying.
-//    UICollectionViewLayoutAttributes *attributes = [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
-//    
-//    if ([self.deletedRowSet containsObject:@(itemIndexPath.item)])
-//    {
-//        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
-//        attributes.alpha = 0.0;
-//        attributes.transform3D = CATransform3DMakeScale(0.1, 0.1, 1.0);
-//        attributes.transform3D = CATransform3DRotate(attributes.transform3D, M_PI_4, 0, 0, 1);
-//        
-//        return attributes;
-//    }
-//    
-//    return attributes;
-//}
+-(void)prepareForCollectionViewUpdates:(NSArray *)updateItems
+{
+    [super prepareForCollectionViewUpdates:updateItems];
+    
+    [updateItems enumerateObjectsUsingBlock:^(UICollectionViewUpdateItem *updateItem, NSUInteger idx, BOOL *stop) {
+        if (updateItem.updateAction == UICollectionUpdateActionInsert)
+        {
+            [self.insertedRowSet addObject:@(updateItem.indexPathAfterUpdate.item)];
+        }
+        else if (updateItem.updateAction == UICollectionUpdateActionDelete)
+        {
+            [self.deletedRowSet addObject:@(updateItem.indexPathBeforeUpdate.item)];
+        }
+    }];
+}
+
+-(void)finalizeCollectionViewUpdates
+{
+    [super finalizeCollectionViewUpdates];
+    
+    [self.insertedRowSet removeAllObjects];
+    [self.deletedRowSet removeAllObjects];
+}
+
+- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    UICollectionViewLayoutAttributes *attributes = [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
+    
+    if ([self.insertedRowSet containsObject:@(itemIndexPath.item)])
+    {
+        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+        attributes.alpha = 0.0;
+        attributes.transform3D = CATransform3DMakeScale(0.1, 0.1, 1.0);
+        attributes.transform3D = CATransform3DRotate(attributes.transform3D, -M_PI_4, 0, 0, 1);
+        return attributes;
+    }
+    
+    return attributes;
+}
+
+- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    // The documentation says that this returns nil. It is lying.
+    UICollectionViewLayoutAttributes *attributes = [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
+    
+    if ([self.deletedRowSet containsObject:@(itemIndexPath.item)])
+    {
+        attributes = [self layoutAttributesForItemAtIndexPath:itemIndexPath];
+        attributes.alpha = 0.0;
+        attributes.transform3D = CATransform3DMakeScale(0.1, 0.1, 1.0);
+        attributes.transform3D = CATransform3DRotate(attributes.transform3D, M_PI_4, 0, 0, 1);
+        
+        return attributes;
+    }
+    
+    return attributes;
+}
 
 @end
